1f2fae549d2023dbe0b271627471094a54a65b32
despresj <54477023+despresj@users.noreply.github.com>
2023-04-04 19:26:47 -0400

    clipboard display and rev string

--- src/gui.rs
+++ src/gui.rs
@@ -21,12 +21,13 @@ impl FileViewer {
         Self::default()
     }
 }
-
+use std::time::Instant;
 impl eframe::App for FileViewer {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let mut file_contents = read_and_rev_file(Path::new(LOGFILE)).into_iter();
 
         egui::CentralPanel::default().show(ctx, |ui| {
+            let start = Instant::now();
             if ui.input(|i| i.key_pressed(egui::Key::Q)) {
                 panic!("BOO Pressed Q");
             }
@@ -34,15 +35,27 @@ impl eframe::App for FileViewer {
                 todo!();
             }
             let current_clipboard = file_contents.next().unwrap().strip_time_stamp();
-            ui.add(egui::Label::new(format!(
-                "Current Clipboard: {current_clipboard}"
-            )));
+            ui.label(
+                egui::RichText::new(format!("Current Clipboard: {current_clipboard}"))
+                    .heading()
+                    .color(egui::Color32::from_rgb(8, 186, 183)),
+            );
             egui::ScrollArea::vertical().show(ui, |ui| {
                 for line in file_contents {
                     if ui.selectable_label(false, &line).clicked() {
+                        ui.push(egui::Event::WindowClosed);
                         copy_item(line);
+                        ctx.request_repaint();
+                        ctx.request_repaint();
+                        ctx.request_repaint();
+                        ctx.request_repaint();
+                        ctx.request_repaint();
+                        ctx.request_repaint();
                     }
                 }
+                let end = Instant::now();
+                let duration = end.duration_since(start);
+                println!("Time elapsed: {:?}", duration);
             });
         });
     }
